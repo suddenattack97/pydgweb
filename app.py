454a4045a50e5e86dab58644d589006b96a57194
minsu <rkdalstn9788@gmail.com>
2023-10-06 15:06:22 +0900

    [도로 API] n빵된 세그먼트 수선의발 좌표 정상 저장되도록 백엔드 getPoint 함수 변경

--- app.py
+++ app.py
@@ -140,33 +140,51 @@ def save_coordinate():
     # # 커서와 연결을 닫음
     # cursor.close()
     # cnx.close()
-    car_id = request.form['car_id']
-    x = request.form['x']
-    y = request.form['y']
+  
+@app.route('/events_foot')
+def sse_foot():
+    def event_stream_foot():
+        last_id = None  # 마지막으로 가져온 데이터의 ID
+        while True:
+            cnx = pymysql.connect(
+                host="localhost",
+                user="root",
+                password="2412",
+                database="road"
+            )
+            cursor = cnx.cursor()
 
-    cnx = pymysql.connect(
-        host="localhost",  # 예: "localhost"
-        user="root",  # 예: "root"
-        password="2412",  # 예: "yourpassword"
-        database="road"  # 연결하려는 데이터베이스 이름. 예: "yourdatabase"
-    )
-    cursor = cnx.cursor()
-    try:
-        # 좌표를 데이터베이스에 저장
-    except Exception as e:
-        print("General error:", e)
-        return jsonify({"status": "error", "message": "General error: {}".format(e)}), 500
-    finally:
-        cursor.close()
-        cnx.close()
+            # 마지막 ID 이후의 새로운 데이터를 가져오기
+            if last_id:
+                query = f"SELECT * from dataset WHERE idx > {last_id} AND footpoint IS NULL ORDER BY idx ASC"
+            else:
+                query = "SELECT * from dataset WHERE footpoint IS NULL ORDER BY idx ASC "
+            #query = "SELECT * FROM dataset WHERE footpoint IS NULL ORDER BY idx ASC"
+            
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            
+            # 새로운 데이터가 있으면 클라이언트에 전송
+            if rows:
+                last_id = rows[-1][0]
+                coordinates = [{"idx": row[0], "xy_date": row[1], "car_id": row[2], "x_point": row[3], "y_point": row[4]} for row in rows]
+                yield f"data: {json.dumps(coordinates)}\n\n"
+            
+            cursor.close()
+            cnx.close()
+            time.sleep(1)  # 1초 간격으로 데이터베이스 조회
 
-@app.route('/save-coordinate', methods=['POST'])
-def save_coordinate():
-    print("@")
-    car_id = request.form['car_id']
-    x = request.form['x']
-    y = request.form['y']
+    return Response(event_stream_foot(), mimetype="text/event-stream")
+    
 
+@app.route('/getfootPoint', methods=['POST'])
+def getfootPoint():
+    data = request.get_json()
+    car_id = data.get('car_id')
+    footPoint = data.get('footPoint')  # 이중 배열을 받아옵니다.
+    ori_x = data.get('ori_x')  # 이중 배열을 받아옵니다.
+    ori_y = data.get('ori_y')  # 이중 배열을 받아옵니다.
+    #print(footPoint)
     cnx = pymysql.connect(
         host="localhost",  # 예: "localhost"
         user="root",  # 예: "root"
@@ -193,17 +211,9 @@ def save_coordinate():
     finally:
         cursor.close()
         cnx.close()
-    # # MySQL 쿼리를 실행하여 road_data 테이블 업데이트
-    # insert_query = "INSERT INTO dataset (x_point, y_point) VALUES (%s, %s)"
-    # print((x, y))
-    # cursor.execute(insert_query, (x, y))
-
-    # # 변경사항을 커밋
-    # cnx.commit()
-
-    # # 커서와 연결을 닫음
-    # cursor.close()
-    # cnx.close()
+        
+    
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4444, debug=True)
